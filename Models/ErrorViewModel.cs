bd14e5e541652992439d4e61aae7a5a7d5509174
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2023-12-11 13:30:19 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HospitalAppointmentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
