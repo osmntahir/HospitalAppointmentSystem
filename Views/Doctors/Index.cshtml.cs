0dba4e9403f1937f0a04de2f454322f6d2505cd0
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2023-12-14 14:21:28 +0300

    Database paketleri indirildi

---
+++ Views/Doctors/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace HospitalAppointmentSystem.Views.Doctors
+{
+    public class Index : PageModel
+    {
+        public void Onget()
+        {
+
+        }
+    }
+}
