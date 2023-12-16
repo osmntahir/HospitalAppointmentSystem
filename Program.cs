08cd659ff1f9f9b520537cc184233bd1eaf199ac
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2023-12-16 18:37:48 +0300

    paket duzenlemeleri

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using HospitalAppointmentSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("HospitalDatabase")));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
